ee0419741fe5a5a31678fa14aa84539bf44b7057
Egor <pregus@rambler.ru>
2020-05-15 11:46:34 +0300

    comments updated

--- go_counter_proc/go_counter_proc.go
+++ go_counter_proc/go_counter_proc.go
@@ -19,23 +19,27 @@ type siteStat struct {
 
 // countGo counts the number of "Go" in the body of response from url
 func countGo(url string) (siteStat, error) {
-	//fmt.Printf("start %s\n", url)
+	// send GET request
 	resp, err := http.Get(url)
 	if err != nil {
 		return siteStat{url, 0}, fmt.Errorf("request to %s failed, %s", url, err)
 	}
 	defer resp.Body.Close()
+	// check the status of the response
 	if resp.StatusCode != http.StatusOK {
 		return siteStat{url, 0}, fmt.Errorf("request to %s failed, %s", url, resp.Status)
 	}
+	// check content type
 	contentType := resp.Header.Get("Content-Type")
 	if contentType != "text/html" && !strings.HasPrefix(contentType, "text/html;") {
 		return siteStat{url, 0}, fmt.Errorf("the type of doc at %s is not text/html but %s", url, contentType)
 	}
+	// put the body of response into buffer
 	resp_body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Errorf("read from body of response from %s error %s", url, err)
 	}
+	// count the number of "Go" instanses in the body of response
 	numGo := bytes.Count(resp_body, []byte("Go"))
 	return siteStat{url, numGo}, nil
 }
@@ -45,6 +49,7 @@ func countGo(url string) (siteStat, error) {
 // returns the total number of "Go" in all urls
 // input parameter k is the number of simultaneousely fetched urls
 func InputCountGo(inpSrc *os.File, k int) int {
+	// set default input parameters if zero values passed
 	if inpSrc == nil {
 		inpSrc = os.Stdin
 	}
@@ -55,7 +60,7 @@ func InputCountGo(inpSrc *os.File, k int) int {
 	numGoList := make(chan int)      // channel to pass number of Go from fetcher to total counter
 	inpListRdy := make(chan bool)    // signal of input list is sent to handling
 	tokens := make(chan struct{}, k) // tokens for limiting simultaneously running goroutines
-	var wg sync.WaitGroup            // waitgroup for waiting running gouroutines
+	var wg sync.WaitGroup            // waitgroup for waiting for running gouroutines are finished
 
 	// start fetching goroutines with limiting of number of goroutines
 	go func() {
@@ -70,6 +75,7 @@ func InputCountGo(inpSrc *os.File, k int) int {
 		}
 	}()
 
+	// read sites statistics and accumulate total nomber fo "Go"
 	tot_num := 0
 	go func() {
 		for num := range numGoList {
@@ -78,6 +84,7 @@ func InputCountGo(inpSrc *os.File, k int) int {
 		}
 	}()
 
+	// fill input channel with input values
 	go func() {
 		scanner := bufio.NewScanner(inpSrc)
 		for scanner.Scan() {
@@ -86,7 +93,9 @@ func InputCountGo(inpSrc *os.File, k int) int {
 		}
 		inpListRdy <- true
 	}()
+	// wait for filling input channel before starting of waiting for output channel is empty
 	<-inpListRdy
+	// wait for all sites was counted
 	wg.Wait()
 	return tot_num
 }
